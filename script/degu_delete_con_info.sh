855ceb89de0c2f119d02e7adde89fd47271e95b4
Keitaro Takahashi <keitaro.takahashi@atmark-techno.com>
2020-01-21 16:00:52 +0900

    Update degu_delete_con_info.sh

--- script/degu_delete_con_info.sh
+++ script/degu_delete_con_info.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 for i in $(find /sys/class/block/sd*/device/model)
 do
-    model=$(cat $i)
-    if [ "$(echo $model)" = "ZEPHYR USB DISK" ] ; then
+    model=$(cat $i | sed 's/\s*$//')
+    if [ "$model" = "ZEPHYR USB DISK" ] ; then
         degu=$(echo $i | sed -e "s/^.*block\/\(.*\)\/device.*/\1/g")
         dd if=/dev/zero of=/dev/$degu bs=1k count=16 seek=16 conv=fsync,nocreat
         cmp -n 16k -i 16k /dev/$degu /dev/zero
